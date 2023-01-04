9c94411d7f8784facbc17e40fa399c5132d97112
Arihantkapoor <62657676+Arihantkapoor@users.noreply.github.com>
2023-01-04 11:43:31 +0530

    Create Minimum rounds to complete all tasks.cpp

---
+++ Minimum rounds to complete all tasks.cpp
@@ -0,0 +1,17 @@
+//Question link:- https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/
+
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int,int> mp;
+        for(auto it: tasks){
+            mp[it]++;
+        }
+        int ans = 0;
+        for(auto &it: mp){
+            if(it.second==1) return -1;
+            else ans+=(it.second+2)/3;
+        }
+        return ans;
+    }
+};
