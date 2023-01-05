9a867ded9e82f1052978784fcb0ddd075b604086
Arihantkapoor <62657676+Arihantkapoor@users.noreply.github.com>
2023-01-05 17:32:01 +0530

    Update Minimum Number of Arrows to burst balloons.cpp

--- Minimum Number of Arrows to burst balloons.cpp
+++ Minimum Number of Arrows to burst balloons.cpp
@@ -2,17 +2,17 @@
 
 class Solution {
 public:
+    static bool cmp(vector<int>&a, vector<int>&b){
+        return a[1]<b[1];
+    }
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
-        int ans = 1;
-        int lp = points[0][1];
-        
+        sort(points.begin(),points.end(),cmp);
+        int ans = 1, end = points[0][1];
         for(auto it: points){
-            if(it[0]>lp){
+            if(it[0]>end){
                 ans++;
-                lp = it[1];
+                end = it[1];
             }
-            lp = min(lp,it[1]);
         }
         return ans;
     }
